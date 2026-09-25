91c78a8a5521bf9adc03d181075c84e2071e1f9f
Pedro <pedroezequiel.limaf@gmail.com>
2026-09-24 23:41:22 -0300

    Feat: Adicionados novos protocolos para autenticação e armazenamento do Firebase/Google

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.ytimg.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
     ],
   },
 };
